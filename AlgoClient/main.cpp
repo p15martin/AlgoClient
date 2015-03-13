6d3199e0e7cd5bf3db95ea1ed7adda82501ddf89
Peter Martin <p15martin@outlook.com>
2015-03-13 15:55:54 -0700

    Initial Commit

---
+++ AlgoClient/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgoClient
+//
+//  Created by Peter Martin on 3/13/15.
+//  Copyright (c) 2015 Peter Martin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
